63e57c462b63bdd8180cb41bc380920cf2841341
crisplayz2019 <crisplayz2019@gmail.com>
2020-10-14 16:01:29 -0400

    Mi mini Proyecto

---
+++ scrol.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu_var").addEventListener("click", () => {
+  document.querySelector(".bar").classList.toggle("show");
+});
+
+
+ScrollReveal().reveal('.container');
+ScrollReveal().reveal('.inicio', { delay: 500 });
+ScrollReveal().reveal('.min-pre', { delay: 500 });
+ScrollReveal().reveal('.Sobre-mi', { delay: 500 });
+ScrollReveal().reveal('.cierre', { delay: 500 });
